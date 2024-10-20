0c07680745322eaa48c0ad123d5b9e83b50d332d
reyatwel <sysdev@gmx.com>
2024-10-20 19:54:04 +0800

    fixed test

--- tests/WeightTest.php
+++ tests/WeightTest.php
@@ -7,5 +7,5 @@ it('can convert kilograms to pounds correctly', function () {
     $kilograms = 100;
     $pounds = Weight::kilograms($kilograms)->toPounds();
 
-    Assert::assertEqualsWithDelta($pounds, 221.46226218487758072297380134503, 0.0000001);
+    Assert::assertEqualsWithDelta($pounds, 220.46226218487758072297380134503, 0.0000001);
 });
