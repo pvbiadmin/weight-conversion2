82b28d8e0142c5e6d324e04ff1da05e2d9c21434
reyatwel <sysdev@gmx.com>
2024-10-21 07:29:00 +0800

    added toStones conversion

--- src/Weight.php
+++ src/Weight.php
@@ -6,7 +6,8 @@ use JetBrains\PhpStorm\Pure;
 
 class Weight
 {
-    private const FACTOR = 0.45359237;
+    private const KG_PER_LB = 0.45359237;
+    private const KG_PER_ST = 6.35029;
 
     #[Pure]
     public static function kilograms(float $kilograms): self
@@ -20,10 +21,22 @@ class Weight
     }
 
     /**
+     * Converts Kilograms to Pounds
+     *
      * @return float
      */
     public function toPounds(): float
     {
-        return $this->kilograms / self::FACTOR;
+        return $this->kilograms / self::KG_PER_LB;
+    }
+
+    /**
+     * Converts Kilograms to Stones
+     *
+     * @return float
+     */
+    public function toStones(): float
+    {
+        return $this->kilograms / self::KG_PER_ST;
     }
 }
--- tests/WeightTest.php
+++ tests/WeightTest.php
@@ -9,3 +9,10 @@ it('can convert kilograms to pounds correctly', function () {
 
     Assert::assertEqualsWithDelta($pounds, 220.46226218487758072297380134503, 0.0000001);
 });
+
+it('can convert kilograms to stones correctly', function () {
+    $kilograms = 100;
+    $pounds = Weight::kilograms($kilograms)->toStones();
+
+    Assert::assertEqualsWithDelta($pounds, 15.747312327468509312173144848503, 0.0000001);
+});
