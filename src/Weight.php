77a6f99a9f4d4b5212c58d163444525d033065e7
reyatwel <sysdev@gmx.com>
2024-10-20 20:25:30 +0800

    added comments for docs

--- src/Weight.php
+++ src/Weight.php
@@ -4,23 +4,22 @@ namespace ReyAtwel\WeightConversion;
 
 use JetBrains\PhpStorm\Pure;
 
-class Weight
-{
+class Weight {
     private const FACTOR = 0.45359237;
 
     #[Pure]
-    public static function kilograms(float $kilograms): self
-    {
+    public static function kilograms(float $kilograms): self {
         return new static($kilograms);
     }
 
-    public function __construct(protected float $kilograms)
-    {
+    public function __construct(protected float $kilograms) {
 
     }
 
-    public function toPounds(): float
-    {
+    /**
+     * @return float
+     */
+    public function toPounds(): float {
         return $this->kilograms / self::FACTOR;
     }
 }
