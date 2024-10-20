48c35a5ca6997aabe60d9f8764477e28ab780a86
pvbiadmin <pvbiadmin@users.noreply.github.com>
2024-10-20 12:25:50 +0000

    Fix styling

--- src/Weight.php
+++ src/Weight.php
@@ -4,22 +4,26 @@ namespace ReyAtwel\WeightConversion;
 
 use JetBrains\PhpStorm\Pure;
 
-class Weight {
+class Weight
+{
     private const FACTOR = 0.45359237;
 
     #[Pure]
-    public static function kilograms(float $kilograms): self {
+    public static function kilograms(float $kilograms): self
+    {
         return new static($kilograms);
     }
 
-    public function __construct(protected float $kilograms) {
+    public function __construct(protected float $kilograms)
+    {
 
     }
 
     /**
      * @return float
      */
-    public function toPounds(): float {
+    public function toPounds(): float
+    {
         return $this->kilograms / self::FACTOR;
     }
 }
